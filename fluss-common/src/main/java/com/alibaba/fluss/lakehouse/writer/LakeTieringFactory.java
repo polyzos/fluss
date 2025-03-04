02e33522c70dd63f36b7a7c83b5e7bf9312065d1
ipolyzos <ipolyzos.se@gmail.com>
2025-03-04 11:16:59 +0200

    move interfaces to common module

--- fluss-common/src/main/java/com/alibaba/fluss/lakehouse/writer/LakeTieringFactory.java
+++ fluss-common/src/main/java/com/alibaba/fluss/lakehouse/writer/LakeTieringFactory.java
@@ -1,26 +1,25 @@
 /*
- * Copyright (c) 2024 Alibaba Group Holding Ltd.
+ *  Copyright (c) 2024 Alibaba Group Holding Ltd.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
-package com.alibaba.fluss.lake.common.writer;
+package com.alibaba.fluss.lakehouse.writer;
 
-import com.alibaba.fluss.lake.common.serializer.SimpleVersionedSerializer;
+import com.alibaba.fluss.lakehouse.serializer.SimpleVersionedSerializer;
 
 import org.apache.flink.api.connector.sink2.Committer;
 import org.apache.flink.api.connector.sink2.CommitterInitContext;
-import org.apache.flink.connector.file.table.stream.compact.CompactWriter;
 
 import java.io.IOException;
 
@@ -41,7 +40,7 @@ public interface LakeTieringFactory<WriteResult, CommitableT> {
      * @return the lake writer
      * @throws IOException if an I/O error occurs
      */
-    CompactWriter<WriteResult> createLakeWriter(WriterInitContext writerInitContext)
+    LakeWriter<WriteResult> createLakeWriter(WriterInitContext writerInitContext)
             throws IOException;
 
     /**
--- fluss-common/src/main/java/com/alibaba/fluss/lakehouse/writer/LakeWriter.java
+++ fluss-common/src/main/java/com/alibaba/fluss/lakehouse/writer/LakeWriter.java
@@ -1,22 +1,22 @@
 /*
- * Copyright (c) 2024 Alibaba Group Holding Ltd.
+ *  Copyright (c) 2024 Alibaba Group Holding Ltd.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
-package com.alibaba.fluss.lake.common.writer;
+package com.alibaba.fluss.lakehouse.writer;
 
-import com.alibaba.fluss.client.table.scanner.ScanRecord;
+import com.alibaba.fluss.record.LogRecord;
 
 import java.io.Closeable;
 import java.io.IOException;
@@ -34,7 +34,7 @@ public interface LakeWriter<WriteResult> extends Closeable {
      * @param record the record to write
      * @throws IOException if an I/O error occurs
      */
-    void write(ScanRecord record) throws IOException;
+    void write(LogRecord record) throws IOException;
 
     /**
      * Completes the writing process and returns the write result.
