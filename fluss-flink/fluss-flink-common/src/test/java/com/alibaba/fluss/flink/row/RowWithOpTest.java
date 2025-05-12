45eafe97ee7fef20096bf029c0e87fb888a45c24
ipolyzos <ipolyzos.se@gmail.com>
2025-05-12 18:59:40 +0300

    fix checkstyle

--- fluss-flink/fluss-flink-common/src/test/java/com/alibaba/fluss/flink/row/RowWithOpTest.java
+++ fluss-flink/fluss-flink-common/src/test/java/com/alibaba/fluss/flink/row/RowWithOpTest.java
@@ -24,7 +24,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-/** Test for {@link @RowWithOp}. */
+/** Test for {@link RowWithOp}. */
 public class RowWithOpTest {
 
     @Test
